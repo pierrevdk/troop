b0fa167ebf454b96896f993e10f04a7aa8cc97cb
Pierre Vandekerckhove <pierre4vdk@gmail.com>
2019-04-30 19:52:34 +0200

    Update towerblocks.js

--- towerblocks.js
+++ towerblocks.js
@@ -20,7 +20,7 @@ var Stage = /** @class */function () {
       alpha: false });
 
     this.renderer.setSize(window.innerWidth, window.innerHeight);
-    this.renderer.setClearColor('#FFFFFF', 1);
+    this.renderer.setClearColor('#ffffff', 1);
     this.container.appendChild(this.renderer.domElement);
     // scene
     this.scene = new THREE.Scene();
